6de7fa55469e64bde5f6aca2048595e72e9b741b
Leonardo Faria <leonardofaria00@gmail.com>
2022-03-31 22:45:02 -0300

    Removed Uuid generator in package

--- src/main/java/br/edu/infnet/votingapi/domain/UuidProvider.java
+++
@@ -1,9 +0,0 @@
-package br.edu.infnet.votingapi.domain;
-
-import java.util.UUID;
-
-public class UuidProvider {
-    public static String get() {
-        return UUID.randomUUID().toString();
-    }
-}
