3e87f2055103c47e00d4308aa5535c4bcee0cf3a
Leonardo Faria <leonardofaria00@gmail.com>
2022-03-31 22:33:55 -0300

    Added LoggerConfiguration

---
+++ src/main/java/br/edu/infnet/votingapi/application/config/LoggerConfiguration.java
@@ -0,0 +1,17 @@
+package br.edu.infnet.votingapi.application.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class LoggerConfiguration {
+    @Bean
+    @Scope("prototype")
+    public Logger logger(final InjectionPoint injectionPoint) {
+        return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass());
+    }
+}
