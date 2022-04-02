96b3732311b645aa42cb9bb6e59abc01fb48ca5b
Leonardo Faria <leonardofaria00@gmail.com>
2022-04-01 22:32:37 -0300

    Created First Test to return Candidates

---
+++ src/test/java/br/edu/infnet/votingapi/application/candidate/CandidateControllerTest.java
@@ -0,0 +1,33 @@
+package br.edu.infnet.votingapi.application.candidate;
+
+import br.edu.infnet.votingapi.domain.data.model.candidate.Candidate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class CandidateControllerTest {
+
+    @Autowired
+    private CandidateController candidateController;
+
+    @Test
+    @DisplayName("Deve retornar uma lista de candidatos")
+    void getCandidates() {
+        ResponseEntity<List<Candidate>> response = candidateController.getCandidates();
+        assertEquals(response.getStatusCode().value(), 200);
+        for (Candidate candidate : Objects.requireNonNull(response.getBody())) {
+            assertNotNull(candidate.getUuid());
+            assertNotNull(candidate.getName());
+            assertNotNull(candidate.getPoliticalParty());
+        }
+    }
+}
