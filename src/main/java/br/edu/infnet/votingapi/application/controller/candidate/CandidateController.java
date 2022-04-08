66bd6734a18cdb5abfe31f35adbd20249b653f84
Leonardo Faria <leonardofaria00@gmail.com>
2022-04-07 21:26:30 -0300

    Updated route

--- src/main/java/br/edu/infnet/votingapi/application/controller/candidate/CandidateController.java
+++ src/main/java/br/edu/infnet/votingapi/application/controller/candidate/CandidateController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/candidate")
+@RequestMapping("/voting/v1/candidate")
 public class CandidateController implements CandidateAPI {
     private final CandidateService candidateService;
     private final CandidateMapper candidateMapper;
