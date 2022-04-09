4ea5e174ee5eb24eb53246b7116d628567618e75
Leonardo Faria <leonardofaria00@gmail.com>
2022-04-09 13:16:38 -0300

    Added Constructor to CandidateRequest.java

--- src/main/java/br/edu/infnet/votingapi/application/data/candidate/CandidateRequest.java
+++ src/main/java/br/edu/infnet/votingapi/application/data/candidate/CandidateRequest.java
@@ -1,10 +1,15 @@
 package br.edu.infnet.votingapi.application.data.candidate;
 
 import br.edu.infnet.votingapi.domain.data.model.candidate.PoliticalParty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CandidateRequest {
+
     private String name;
     private PoliticalParty politicalParty;
 
