dd38536d123a534b05d8c6a63783387ebae34064
Leonardo Faria <leonardo.faria@e-unicred.com.br>
2022-11-30 22:02:04 -0300

    Added Eureka Client

---
+++ src/main/java/br/edu/infnet/votingapi/infrastructure/eureka/config/EurekaClientConfiguration.java
@@ -0,0 +1,9 @@
+package br.edu.infnet.votingapi.infrastructure.eureka.config;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaClient
+public class EurekaClientConfiguration {
+}
